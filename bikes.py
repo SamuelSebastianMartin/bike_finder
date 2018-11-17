d993d71383bafb6ddd08a0f8e5b13e1a08c20210
Samuel Martin <Samuel Martin>
2018-11-17 21:14:35 +0000

    text based version working well, but showing only bikes-availabe, not spaces. Functionality for spaces is all there, though.

--- bikes.py
+++ bikes.py
@@ -7,32 +7,54 @@ Then parses it to recover only the Terminal Name & Number or Bikes.
 If I feel good, it will be GUI.
 '''
 
-import requests  # For dealing with url.
-import xml.etree.ElementTree as ET
 
-###     Downloading     ###
+def main():
+    whole_xml = get_xml()
+    location = "St. John's Wood"
+    data = parse_xml(whole_xml, location)
+    print()
+    print("Bikes available near St. John's Wood: ")
+    print()
+    for datum in data:
+        print(datum[0], datum[1])
 
-url = 'https://tfl.gov.uk/syndication/feeds/cycle-hire/livecyclehireupdates.xml'
-page = requests.get(url)
-# Check that the data has arrived.
-try:
-    page.raise_for_status()
-except Exception as exc:
-    print('There was a problem: %s' % (exc))
-
-whole_xml = page.text  # This is the relevant .xml data.
+    location = "Bloomsbury"
+    data = parse_xml(whole_xml, location)
+    print()
+    print("Bikes available near Bloomsbury: ")
+    print()
+    for datum in data:
+        print(datum[0], datum[1])
 
 
-###     Parsing     ###
-root = ET.fromstring(whole_xml)
-for station in root:
-    if "St. John's Wood" in station[1].text:
-        print()
-        print(station[1].text)
-        print(station[10].text)
+def get_xml():
+    import requests  # For dealing with url.
+    url = 'https://tfl.gov.uk/syndication/feeds/cycle-hire/livecyclehireupdates.xml'
+    page = requests.get(url)
+    # Check that the data has arrived.
+    try:
+        page.raise_for_status()
+    except Exception as exc:
+        print('There was a problem: %s' % (exc))
 
-    if "Bloomsbury" in station[1].text:
-        print()
-        print(station[1].text)
-        print(station[10].text)
+    whole_xml = page.text  # This is the relevant .xml data.
+    return whole_xml
 
+
+def parse_xml(whole_xml, location):
+    import xml.etree.ElementTree as ET
+    root = ET.fromstring(whole_xml)
+
+    data = []
+    for station in root:
+        if location in station[1].text:
+            name = station[1].text
+            bikes = station[10].text
+            spaces = station[11].text
+            datum = (name, bikes, spaces)
+            data.append(datum)
+    return data
+
+
+if __name__ == '__main__':
+    main()
